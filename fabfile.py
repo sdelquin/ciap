54de71d5abd7da88ae0995b6d7f9c409fd066f01
Sergio Delgado Quintero <sdelquin@gmail.com>
2022-12-08 12:35:20 +0000

    Fix deploy script

--- fabfile.py
+++ fabfile.py
@@ -1,13 +1,13 @@
 from fabric.api import cd, env, local, prefix, run
 
-env.hosts = ['cloud']
+env.hosts = ['sdelquin.me']
 
 
 def deploy():
     local('git push')
     with cd('~/code/ciap'):
         run('git pull')
-        with prefix('source ~/.virtualenvs/ciap/bin/activate'):
+        with prefix('source ~/.pyenv/versions/ciap/bin/activate'):
             with cd('~/code/ciap'):
                 run('pip install -r requirements.txt')
                 run('bower install')
